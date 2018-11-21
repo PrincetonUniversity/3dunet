7407dda555ac5b33b91460ad3c7268cde1becfb5
zahramansoor <zahramansoor@github.com>
2018-11-21 17:00:48 -0500

    autopush

--- slurm_scripts/cnn_step1.sh
+++ slurm_scripts/cnn_step1.sh
@@ -6,7 +6,7 @@
 #SBATCH -o logs/cnn_step1_%j.out        # STDOUT #add _%a to see each array job
 #SBATCH -e logs/cnn_step1_%j.err        # STDERR #add _%a to see each array job
 #SBATCH --contiguous #used to try and get cpu mem to be contigous
-#SBATCH --mem 25000 #25 gbs
+#SBATCH --mem 30000 #30 gbs
 
 echo "In the directory: `pwd` "
 echo "As the user: `whoami` "
