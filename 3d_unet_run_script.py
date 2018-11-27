bab6e3110aa119aa42f2a5be00311d7647509a6b
zahramansoor <zahramansoor@github.com>
2018-11-27 10:27:38 -0500

    autopush

---
+++ 3d_unet_run_script.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 08:32:24 2018
+
+@author: tpisano
+"""
+
+#https://github.com/PrincetonUniversity/3dunet/blob/master/run_chnk_fwd.sh
+#https://github.com/PrincetonUniversity/3dunet/blob/master/pytorchutils/run_chnk_fwd.py
+
+#repull repo to tiger2 (gpu)
+#https://github.com/PrincetonUniversity/3dunet
+#git clone https://username:password@github.com/PrincetonUniversity/3dunet.git
+
+#to activate
+#module load cudatoolkit/10.0 cudnn/cuda-10.0/7.3.1 anaconda3/5.2.0
+#. activate 3dunet
+
+
+#transfer from PNI scratch to tigress using globus https://app.globus.org/file-manager
+import os
+inn = '/jukebox/scratch/zmd/for_tom/'
+out = '/tigress/tpisano/3dunet_data/'
+logs = os.path.join(out, 'logs')
+if not os.path.exists(logs): os.mkdir(logs)
+repo = '/tigress/tpisano/3dunet'
+slurm_script = 'run_chnk_fwd.sh'
+tom_folder = os.path.join(out, 'tom_slurm_scripts')
+if not os.path.exists(tom_folder): os.mkdir(tom_folder)
+print(tom_folder)
+
+#function to run
+from subprocess import check_output
+def sp_call(call):
+    print(check_output(call, shell=True))
+    return
+
+
+#loop
+paths = [xx for xx in os.listdir(out) if 'experiments' not in xx and 'logs' not in xx and 'tom_slurm_scripts' not in xx]
+paths_to_call = []
+for pth in paths:
+    
+    #read file
+    with open(os.path.join(repo, slurm_script), 'r') as fl:
+        lines = fl.readlines()
+        fl.close()
+    
+    #modify
+    new_lines = lines[:]
+    for i,line in enumerate(lines):
+        
+        #new dir
+        if 'cd pytorchutils/' in line:
+            new_lines[i] = line.replace('pytorchutils/', '/tigress/tpisano/3dunet/pytorchutils/')
+        
+        #path to folder
+        if 'python run_chnk_fwd.py 20181115_zd_train' in line:
+            new_lines[i] = line.replace('20180327_jg42_bl6_lob6a_05', pth)
+            
+        #logs
+        if 'logs/chnk_' in line:
+            new_lines[i] = line.replace('logs', logs)
+            
+    #save out
+    with open(os.path.join(tom_folder, 'run_'+pth+'.sh'), 'w+') as fl:
+        [fl.write(new_line) for new_line in new_lines]
+        fl.close()
+        
+    #collect
+    paths_to_call.append(os.path.join(tom_folder, 'run_'+pth+'.sh'))
+    
+#call
+for pth in paths_to_call:
+    call = 'sbatch --array=0-100 {}'.format(pth)
+    print(call)
+    sp_call(call)
+    
