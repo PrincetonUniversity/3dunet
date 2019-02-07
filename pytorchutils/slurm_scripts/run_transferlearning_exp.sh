453ef7db5a85be4f2d51cd57e97c68445afd4406
Zahra <zahramansoor@github.com>
2019-02-07 17:56:51 -0500

    adding slurm scripts

--- pytorchutils/slurm_scripts/run_transferlearning_exp.sh
+++ pytorchutils/slurm_scripts/run_transferlearning_exp.sh
@@ -7,9 +7,9 @@
 #SBATCH --contiguous
 #SBATCH --mem=14000 #14gbs
 #SBATCH -t 8500                 # time (minutes)
-#SBATCH -o cnn_train_20190130.out
-#SBATCH -e cnn_train_20190130.err
+#SBATCH -o cnn_tl_train_20190130.out
+#SBATCH -e cnn_tl_train_20190130.err
 
 module load cudatoolkit/10.0 cudnn/cuda-10.0/7.3.1 anaconda3/5.3.1
 . activate 3dunet
-python run_exp.py 20190130_zd_train models/RSUNet.py samplers/soma.py augmentors/flip_rotate.py --batch_sz 18 --gpus 0,1,2,3
+python run_exp.py 20190130_zd_transfer_learning models/RSUNet.py samplers/soma.py augmentors/flip_rotate.py --batch_sz 18 --chkpt_num 295590 --gpus 0,1,2,3
