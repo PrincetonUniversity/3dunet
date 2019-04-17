f703a3d3597f6819990fca78789a53a81ea8615b
Zahra <zahramansoor@github.com>
2019-04-17 13:06:43 -0400

    autopush

--- run_bulk_chnk_fwd.py
+++ run_bulk_chnk_fwd.py
@@ -19,7 +19,7 @@ def sp_call(call):
 
 
 #loop
-paths = [xx for xx in os.listdir(out) if "chck_errs.sh" not in xx and "split_data" not in xx and "logs" not in xx and "slurm_scripts" not in xx]
+paths = [xx for xx in os.listdir(out) if "chck_errs.sh" not in xx and "save" not in xx and "logs" not in xx and "slurm_scripts" not in xx and "output_chnks" not in os.listdir(os.path.join(out, xx))]
 paths_to_call = []
 for pth in paths:
     
